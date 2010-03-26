bb224fca5cfc70cd17fcf45f2ee7c395165d2289
Yann Hodique <yann.hodique@gmail.com>
2010-03-26 17:27:21 +0100

    rtm test

---
+++ test/test_rtm.py
@@ -0,0 +1,18 @@
+from mocker import MockerTestCase
+
+from rtmlib.rtm import Rtm
+
+class TestRtmSanity(MockerTestCase):
+
+    def setUp(self):
+        self.rtm = Rtm(apiKey="dummy key",
+                       secret="dummy secret",
+                       token="dummy token")
+
+    def testAccessors(self):
+        self.assertTrue(callable(self.rtm.lists.delete))
+        self.assertTrue(callable(self.rtm.auth.getToken))
+
+TestRtmSanity.status = "unstable"
+TestRtmSanity.component = "lib"
+TestRtmSanity.lib = "rtm"
